556ac398fe0e4102930d41cec0c04d42ea75a57a
ClaireQingyingHuang <159375480+ClaireQingyingHuang@users.noreply.github.com>
2024-09-13 07:42:48 +0000

    version 2

--- IntegerArrayList.java
+++ IntegerArrayList.java
@@ -27,23 +27,41 @@ public class IntegerArrayList implements IntegerList{
             temp[a] = data[i];
             a++;
         }
-        Integer[] biggerArray = new Integer[data.length*2];
-        for (int i=0; i<index; i++)
-            biggerArray[i] = data[i];
-        biggerArray.add(val);
-        data = biggerArray + temp;
+        if (size+1 >= data.length)
+        {
+            Integer[] biggerArray = new Integer[data.length*2];
+            for (int i=0; i<index; i++)
+                biggerArray[i] = data[i];
+            biggerArray[index] = val;
+            data = biggerArray + temp;
+        }
+        else
+        {
+            for (int i=index; i<size-2; i++)
+                temp = data[i+1];
+                data[i+1] = data[i];
+                data[i+2] = temp;
+            data[index] = val;
+        }
+        
     }
 
     public void set(int index, Integer val){
-
+        if (index<0 || index>=size)
+            throw new IndexOutOfBoundsException("index: " + index);
+        data[index] = val;
     }
 
     public void clear(){
-
+        size = 0;
     }
 
     public void remove(int index){
-
+        if (index<0 || index>=size)
+            throw new IndexOutOfBoundsException("index: " + index);
+        for (int i=index; i<size-1; i++)
+            data[i] = data[i+1];
+        size-=1;
     }
 
     public Integer get(int index){
@@ -53,7 +71,38 @@ public class IntegerArrayList implements IntegerList{
     }
 
     public int size(){
-        return null;
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if (size==0)
+            return true;
+        return false;
+    }
+
+    public boolean contains(int val){
+        for (int i=0; i<size; i++)
+            if (data[i]==val)
+                return true;
+        return false;
+    }
+
+    public int indexOf(int val){
+        for (int i=0; i<size; i++)
+            if (data[i]==val)
+                return i;
+        return -1;
+    }
+
+    public boolean equals(IntegerArrayList other){
+        if (other.size != size)
+            return false;
+        for (int i=0; i<size; i++)
+        {
+            if (data[i].equals(other[i])==false)
+                return false;
+        }
+        return true;
     }
 
     public String toString(){
